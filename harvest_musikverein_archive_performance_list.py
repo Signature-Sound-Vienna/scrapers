7712ea7a926dc4a7f554f5bc241865729dfadbc9
David M. Weigl <weigl@mdw.ac.at>
2021-12-20 16:36:21 +0100

    Update scrapers for new Vienna Philharmonic website

--- harvest_musikverein_archive_performance_list.py
+++ harvest_musikverein_archive_performance_list.py
@@ -9,42 +9,39 @@ import time
 searchterms = ["Neujahrskonzert", "Silvesterkonzert"]
 
 driver = webdriver.Chrome("/usr/bin/chromedriver")
-
 performances = set()
+
+driver.get("https://www.wienerphilharmoniker.at/de/konzert-archiv")
+# accept cookies to get rid of modal mask
+driver.find_element_by_class_name("accept-cookie-settings").click()
+# click on the "start date" year, set to 1939
+driver.find_element_by_id("startYear").click()
+driver.find_element_by_id("startDate.year").send_keys("1939", Keys.RETURN)
+time.sleep(1)
+# click on the "end date" year, set to last value
+driver.find_element_by_id("endYear").click()
+driver.find_element_by_id("endDate.year").send_keys(time.localtime().tm_year, Keys.RETURN)
+time.sleep(1)
 for st in searchterms:
-    driver.get("https://www.wienerphilharmoniker.at/konzerte/archive/")
-    elem = driver.find_element_by_id("dnn_ctr1820_View_stp_SearchTabbedPanel_ctl00_txt_SearchTerm")
-    elem.clear()
-    elem.send_keys(st)
-    elem.send_keys(Keys.RETURN)
+    # open the filter menu
+    driver.find_element_by_class_name("filter-button").click()
+    # fold out the concert title menu
+    driver.find_element_by_class_name("konzerttitel").click()
+    # select any previous input to overwrite it
+    driver.find_element_by_id("konzerttitel").send_keys(Keys.CONTROL, "a")
+    # enter the search term
+    driver.find_element_by_id("konzerttitel").send_keys(st, Keys.RETURN)
+    time.sleep(1)
+    # return from filter menu
+    driver.find_element_by_class_name("return-filter").click()
     element = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.ID, "eventListEntryContainer"))
+        EC.presence_of_element_located((By.ID, "content"))
     )
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="lxml")
-    currentPage = soup.select_one("#dnn_ctr1820_View_hf_CurrentPage").attrs["value"]
-    maxPage = soup.select_one("#dnn_ctr1820_View_hf_MaxPage").attrs["value"]
-    while True:
-        # scrape performances on page
-        for perf in soup.select('#eventListEntryContainer a'):
-            performances.add(perf['href'])
-        if int(currentPage) < int(maxPage)-1:
-            # politely go to next page
-            time.sleep(1)
-            driver.find_element_by_id("dnn_ctr1820_View_hl_PageTopForward").click()
-            time.sleep(5) # hack - should wait for DOM update instead, but below doesnt work
-#            WebDriverWait(driver, 30).until(
-#                EC.text_to_be_present_in_element_value(
-#                    "#dnn_ctr1820_View_hf_CurrentPage", str(int(currentPage)+1)
-#                )
-#            )
-            content = driver.page_source
-            soup = BeautifulSoup(content, features="lxml")
-            currentPage = soup.select_one("#dnn_ctr1820_View_hf_CurrentPage").attrs["value"]
-            print(currentPage, maxPage)
-        else: 
-            # all pages scraped
-            break
-perfList = [perf.replace("from-search/True", "") for perf in list(performances)]
+    perf_anchors = driver.find_elements_by_css_selector(".event-module.selected h2 a")
+    perfs = [perf.get_attribute("href") for perf in perf_anchors]
+    performances.update(perfs)
+
+print("Number of performances found: ", len(performances))
+
 with open("performances.txt", "w") as perfFile:
-    perfFile.write("\n".join(perfList))
+    perfFile.write("\n".join(list(performances)))
--- harvest_musikverein_archive_performances.py
+++ harvest_musikverein_archive_performances.py
@@ -1,12 +1,14 @@
 import argparse
 import requests
 import time 
+import sys
 from uuid import uuid4
 from pprint import pprint
 from bs4 import BeautifulSoup
 from rdflib import RDF, FOAF, Namespace, Graph, URIRef, Literal, BNode
 import urllib
 from urllib import parse
+from urllib.parse import parse_qs, quote
 
 baseUri = "http://localhost:9999"
 
@@ -35,37 +37,77 @@ with open (args.perffile) as pf:
                 time.sleep(1)
         # fetch succeeded, attempt to parse
         try: 
-            perfUri = URIRef(line.strip().rstrip("/"))
+            perfUri = URIRef(line.strip())
+            perfId = line.strip().rstrip("/").rsplit("/",1)[1]
             g.add(( perfUri, RDF.type, mo.Performance ))
-            datetimeloc_h4 = soup.select(".event_properties h4")
-            g.add(( perfUri, dct.date, Literal(datetimeloc_h4[0].text.strip()) ))
-            g.add(( perfUri, ny.timeOfDay, Literal(datetimeloc_h4[1].text.strip()) ))
-            datetimeloc_p = soup.select_one(".event_properties p").text.strip().split("\r\n")
-            g.add(( perfUri, ny.location, Literal(datetimeloc_p[0].strip()) ))
-            g.add(( perfUri, ny.venue, Literal(datetimeloc_p[2].strip()) ))
-            g.add(( perfUri, dct.title, Literal(soup.select_one(".DIV_MODULECONTAINER_threequarter h1").text.strip() )))
+            googleCalendarLink = soup.select(".calendar+.menu-wrapper a")[1]["href"]
+            # extract a date-ish string like 20110101T000000 from the Google Calendar link
+            d = parse_qs(googleCalendarLink)["dates"][0].split("/")[0]
+            # reform into ISO 8601 (yyyy-mm-dd)
+            date = "-".join([d[0:4], d[4:6], d[6:8]])
+            g.add(( perfUri, dct.date, Literal(date) ))
+            # extract venue string
+            venue = soup.select_one(".location").text
+            g.add(( perfUri, ny.venue, Literal(venue) ))
+            # extract title of event
+            title = soup.select_one("h1").text
+            g.add(( perfUri, dct.title, Literal(title) ))
 
-            agent_roles = soup.select(".DIV_MODULECONTAINER_threequarter p")
-            for agent_role in agent_roles:
-                job = agent_role.select_one("job")
-                if job:
-                    g.add(( perfUri, URIRef(baseUri + "/vocab/" + urllib.parse.quote(job.text.strip())), Literal(agent_role.select_one("name").text.strip()) ))
+            # work through agent roles
+            agent_roles_div = soup.select_one(".programm-info div:nth-child(1)") 
+            agent_roles = agent_roles_div.select(".entry .subhead")
+            for role in agent_roles:
+                job = role.text
+                # find all the agents who do this job
+                # (by iterating through sublines until we run out of them)
+                sibling = role.find_next_sibling()
+                while sibling:
+                    if 'subline' in sibling.attrs['class']:
+                        g.add(( perfUri, URIRef(baseUri + "/vocab/" + quote(job)), Literal(sibling.text) ))
+                        sibling = sibling.find_next_sibling()
+                    else:
+                        break
 
-            programmeItems = soup.select("#dnn_ctr800_View_divProgramPane .DIV_MODULECONTAINER_half p:not(.P_pause)")
+            # get programme items
+            programme = soup.select_one(".programm-info div:nth-child(2) .entry .subhead")
+            if programme.text != "Programm":
+                print("WARNING: UNEXPECTED PROGRAM CONTENT:", programme.text, perfUri)
+            # work through programme items
+            # expectation is that we always have one composer and one work title
+            # do some rudimentary validation to check this
+            sibling = programme.find_next_sibling()
+            last_seen = "" 
             itemNum = 1
-            for item in programmeItems:
-                programmeItemUri = URIRef(baseUri + "/programmeItems/" + str(uuid4()))
-                g.add(( programmeItemUri, RDF.type, ny.ProgrammeItem ))
-                g.add(( programmeItemUri, dct.isPartOf, perfUri))
-                g.add(( programmeItemUri, dct.creator, Literal(item.select_one("name").text.strip()) ))
-                item.select_one("name").decompose() # throw out composer name (decompose, ha ha)
-                remainder = item.text.replace("\n", "").split("\r")
-                titles = [title for title in remainder if title]
-                for title in titles:
-                    g.add(( programmeItemUri, dct.title, Literal(title) ))
-                    g.add(( programmeItemUri, ny.programmeItemNumber, Literal(itemNum) ))
-                    itemNum += 1 
-        except:
+            while(sibling):
+                if 'subline' in sibling.attrs['class'] and 'pause' in sibling.attrs['class']:
+                    # intermission, ignore
+                    pass
+                elif 'subline' in sibling.attrs['class'] and 'cast-programm' in sibling.attrs['class']:
+                    if last_seen == "work":
+                        print("WARNING: Multiple works in a row. Something fishy?", perfUri, sibling.text)
+
+                    if creator:
+                        # found a work
+                        programmeItemUri = URIRef(baseUri + "/programmeItems/" + perfId + "/" + str(itemNum))
+                        g.add(( programmeItemUri, RDF.type, URIRef(ny.ProgrammeItem) ))
+                        g.add(( programmeItemUri, dct.isPartOf, URIRef(perfUri) ))
+                        g.add(( programmeItemUri, dct.title, Literal(sibling.text) ))
+                        g.add(( programmeItemUri, dct.creator, Literal(creator) ))
+                        g.add(( programmeItemUri, ny.programmeItemNumber, Literal(itemNum) ))
+                        itemNum += 1
+                    else:
+                        print("WARNING: WORK WITHOUT CREATOR", perfUri, sibling.text)
+                    last_seen = "work"
+                elif 'subline' in sibling.attrs['class']:
+                    if last_seen == "composer":
+                        print("WARNING: Multiple composers in a row. Something fishy?", perfUri, sibling.text)
+                    creator = sibling.text
+                    last_seen = "composer"
+                else:
+                    print("WARNING: unexpected sibling in programme items", perfUri, sibling.attrs["class"])
+                sibling = sibling.find_next_sibling()
+        except Exception:
             print("Failed to parse: ", line.strip())
-        time.sleep(3)
+            print(sys.exc_info())
+        time.sleep(1)
 g.serialize(destination=args.outfile, format="turtle")
